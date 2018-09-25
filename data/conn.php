ba39d04700c5aea05364a4e5bc3cd581b4d2e5a6
ganesh khatri <kh6ganesh@gmail.com>
2018-09-25 16:52:30 +0545

    database username and password setup for local

--- data/conn.php
+++ data/conn.php
@@ -10,9 +10,9 @@
 		
 		function Dbconn(){
 			$this->host = "localhost";
-			$this->uname = "root"; 		
-			$this->psw = "";					
-			$this->dbname = "doasoil";
+			$this->uname = "student"; 		
+			$this->psw = "student";					
+			$this->dbname = "chapagain";
 			
 			$this->links = mysql_connect($this->host,$this->uname,$this->psw) or die("Sorry, couldnot connect to MySQL Server");
 			$this->db = mysql_select_db($this->dbname,$this->links) or die("Sorry, couldnot find database");	
